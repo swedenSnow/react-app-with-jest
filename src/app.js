d52b031169a907fefc44d1ffb1b07d42121a3072
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-10-07 17:45:39 +0200

    authReducer

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { firebase } from './firebase/firebase';
 
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import AppRouter, { history } from './routers/AppRouter';
 
@@ -33,6 +33,8 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
+    store.dispatch(login(user.uid));
+    // console.log('uid', user.uid);
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -40,6 +42,7 @@ firebase.auth().onAuthStateChanged(user => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
