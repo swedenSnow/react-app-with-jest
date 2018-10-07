de157adead8a5631e79d5e51cd7f44a8ed0e22d2
Hani Abu Zeid <31956031+swedenSnow@users.noreply.github.com>
2018-10-07 14:16:08 +0200

    Setup final test suite and added fetch

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import firebase from './firebase/firebase';
 
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import AppRouter from './routers/AppRouter';
@@ -21,4 +21,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
