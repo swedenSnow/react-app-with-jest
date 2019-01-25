2128d9a3ba9f11538d491ed4c5dc4ccd34c17c9f
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-01-25 11:16:34 +0100

    intregration test bug

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -4,22 +4,22 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let startAddExpense, history, wrapper;
+let startAddExpense; let history; let wrapper;
 
 beforeEach(() => {
-  startAddExpense = jest.fn();
-  history = { push: jest.fn() };
-  wrapper = shallow(
-    <AddExpensePage startAddExpense={startAddExpense} history={history} />
-  );
+    startAddExpense = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(
+        <AddExpensePage startAddExpense={startAddExpense} history={history} />
+    );
 });
 
 test('should render AddExpensePage correct', () => {
-  expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit correct', () => {
-  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
-  expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
