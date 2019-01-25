9802ad1aadf7a940a593d9f6d2e69764c6212fba
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-01-25 10:50:59 +0100

    refactor and starting testing circle ci

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <Link className="list-item" to={`/edit/${id}`}>
-    <div>
-      <h3 className="list-item__title">{description}</h3>
-      <span className="list-item__subtitle">
-        {' '}
-        {moment(createdAt).format('MMMM Do, YYYY')}
-      </span>
-    </div>
-    <h3 className="list-item__data">
-      {numeral(amount / 100).format('$0,0.00')}
-    </h3>
-  </Link>
+const ExpenseListItem = ({ id, description, amount, createdAt, note }) => (
+    <Link className="list-item" to={`/edit/${id}`}>
+        <div>
+            <h3 className="list-item__title">{description}</h3>
+            <span className="list-item__subtitle">
+                {' '}
+                {moment(createdAt).format('MMMM Do, YYYY')}
+            </span>
+            {/* <span>{note}</span> */}
+        </div>
+        <h3 className="list-item__data">
+            {numeral(amount / 100).format('$0,0.00')}
+        </h3>
+    </Link>
 );
 export default ExpenseListItem;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,15 +7,15 @@ import authReducer from '../reducers/auth';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
-  const store = createStore(
-    combineReducers({
-      expenses: expensesReducer,
-      filters: filtersReducer,
-      auth: authReducer,
-    }),
-    composeEnhancers(applyMiddleware(thunk))
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-  return store;
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer,
+            auth: authReducer,
+        }),
+        composeEnhancers(applyMiddleware(thunk))
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    return store;
 };
-//Store creation
+// Store creation
