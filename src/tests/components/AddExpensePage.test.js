ded42a27e985b600716a9deab8ff66f153fd9ff5
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-01-26 01:01:17 +0100

    testing with wrong expect(values) to see if Circle ci picks it up

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -16,12 +16,12 @@ beforeEach(() => {
     );
 });
 
-test('shouldd render AddExpensePage correct', () => {
+test('should render AddExpensePage correct', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit correct', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
+    expect(startAddExpense).toHaveBeenLastCalledWith(expenses[2]);
 });
