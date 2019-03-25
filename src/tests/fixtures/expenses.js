b9572fdddabf56ada82ee30afd37da38ec76d568
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-03-25 11:10:06 +0100

    failed testing

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -1,29 +1,29 @@
 import moment from 'moment';
 
 export default [
-  {
-    id: '1',
-    description: 'Gum',
-    note: '',
-    amount: 195,
-    createdAt: 0,
-  },
-  {
-    id: '2',
-    description: 'Huckleberry',
-    note: '',
-    amount: 109500,
-    createdAt: moment(0)
-      .subtract(4, 'days')
-      .valueOf(),
-  },
-  {
-    id: '3',
-    description: 'Blueberry',
-    note: '',
-    amount: 4500,
-    createdAt: moment(0)
-      .add(4, 'days')
-      .valueOf(),
-  },
+    {
+        id: '1',
+        description: 'Gum',
+        note: '',
+        amount: 195,
+        createdAt: 0,
+    },
+    {
+        id: '2',
+        description: 'Huckleberry',
+        note: '',
+        amount: 109500,
+        createdAt: moment(0)
+            .subtract(4, 'days')
+            .valueOf(),
+    },
+    {
+        id: '3',
+        description: 'Blueberry',
+        note: '',
+        amount: 4500,
+        createdAt: moment(0)
+            .add(4, 'days')
+            .valueOf(),
+    },
 ];
